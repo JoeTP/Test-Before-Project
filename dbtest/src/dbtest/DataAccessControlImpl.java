d6b7a02e7e1b6f2a80a651021f5071418a958b23
Ayat Gamal <ayat.ga106@gmail.com>
2025-01-01 23:40:21 +0200

    Delete Function()

--- dbtest/src/dbtest/DataAccessControlImpl.java
+++ dbtest/src/dbtest/DataAccessControlImpl.java
@@ -5,10 +5,26 @@
  */
 package dbtest;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author Youssif
  */
-public class DataAccessControlImpl {
-    
+public class DataAccessControlImpl implements DataAccessControl{
+     
+        public boolean delete(User user) throws SQLException{
+            System.out.println("Implementation of Delete Function,,,,");
+            System.out.println("Implementation of Delete Function,,,,");
+            System.out.println("Implementation of Delete Function,,,,");
+            System.out.println("Implementation of Delete Function,,,,");
+            System.out.println("AY");
+            return false;
+        };
+
+    @Override
+    public boolean insert(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
